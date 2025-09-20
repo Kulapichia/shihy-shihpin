29e4736d32118deb49ef4ff0d1b984f418c8fa51
Kulapichia <chkl@bu.edu>
2025-09-21 04:42:33 +0800

    Update VirtualSearchGrid.tsx

--- src/components/VirtualSearchGrid.tsx
+++ src/components/VirtualSearchGrid.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { FixedSizeGrid as Grid } from 'react-window';
-import type { GridCellProps } from 'react-window';
+import { FixedSizeGrid as Grid, GridOnItemsRenderedProps } from 'react-window';
 import { SearchResult } from '@/lib/types';
 import VideoCard, { VideoCardHandle } from '@/components/VideoCard';
 import DoubanCardSkeleton from './DoubanCardSkeleton';
