62bfc1335f3327454f0ca359edbec2304ee9c279
Kulapichia <chkl@bu.edu>
2025-09-21 04:42:02 +0800

    Update VirtualDoubanGrid.tsx

--- src/components/VirtualDoubanGrid.tsx
+++ src/components/VirtualDoubanGrid.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { FixedSizeGrid as Grid } from 'react-window';
-import type { GridCellProps } from 'react-window';
+import { FixedSizeGrid as Grid, GridOnItemsRenderedProps } from 'react-window';
 import InfiniteLoader from 'react-window-infinite-loader';
 import { DoubanItem } from '@/lib/types';
 import VideoCard from './VideoCard';
